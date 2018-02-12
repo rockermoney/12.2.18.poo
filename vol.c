8ea1ae499b6ad1803b448d8ea21de8e88cd393ba
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-02-12 16:30:42 +0530

    vol.c

---
+++ vol.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) 
+{
+	int a,b,c;
+  printf("enter the a,b,c  value\n");
+  scanf("%d%d%d",&a,&b,&c);
+	int volume;
+	volume=a*b*c;
+	printf("%d",volume);
+	return 0;
+}
